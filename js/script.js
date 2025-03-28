58eec159a6e07965a1206d454d13eb7ecff58338
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-28 14:34:15 +0100

    [UPD] Snack 1: array of objects created

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// snack 1
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
+
+// creazione array di oggetti bikes
+const bikes = [
+    {
+        name: 'bici 1',
+        weight: 10
+    },
+    {
+        name: 'bici 2',
+        weight: 20
+    },
+    {
+        name: 'bici 3',
+        weight: 30
+    }
+]
